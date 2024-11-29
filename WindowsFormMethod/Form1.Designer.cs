58b71e1300a78c0298f5e413db6fbc6ac2e17039
PannawatTH333 <Plamthailand2546@gmail.com>
2024-11-30 02:15:06 +0700

    Add project files.

---
+++ WindowsFormMethod/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace WindowsFormMethod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnBMI = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(281, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "โปรแกรมคำนวณค่า MBI";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label3.Location = new System.Drawing.Point(40, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ส่วนสูง ( cm )";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label2.Location = new System.Drawing.Point(40, 258);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "น้ำหนัก ( kg )";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label4.Location = new System.Drawing.Point(99, 171);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "อายุ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label5.Location = new System.Drawing.Point(49, 124);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "ชื่อ-นามสกุล";
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.Location = new System.Drawing.Point(154, 244);
+            this.txtWeight.Multiline = true;
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(202, 34);
+            this.txtWeight.TabIndex = 4;
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(154, 197);
+            this.txtHeight.Multiline = true;
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(202, 35);
+            this.txtHeight.TabIndex = 3;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(154, 157);
+            this.txtAge.Multiline = true;
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(202, 34);
+            this.txtAge.TabIndex = 2;
+            this.txtAge.TextChanged += new System.EventHandler(this.txtAge_TextChanged);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(154, 113);
+            this.txtName.Multiline = true;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(202, 31);
+            this.txtName.TabIndex = 1;
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            // 
+            // btnBMI
+            // 
+            this.btnBMI.Location = new System.Drawing.Point(263, 331);
+            this.btnBMI.Name = "btnBMI";
+            this.btnBMI.Size = new System.Drawing.Size(93, 54);
+            this.btnBMI.TabIndex = 6;
+            this.btnBMI.Text = "ค่า BMI";
+            this.btnBMI.UseVisualStyleBackColor = true;
+            this.btnBMI.Click += new System.EventHandler(this.btnBMI_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.lblResult.Location = new System.Drawing.Point(391, 122);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(379, 263);
+            this.lblResult.TabIndex = 11;
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(123, 331);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(92, 54);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "ลบ";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnBMI);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtWeight);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnBMI;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ WindowsFormMethod/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormMethod
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("ยินดีต้อนรับทุกท่าน", "โปรแกรมคำนวณค่า MBI");
+            clearForm();    
+        }
+
+        private void btnBMI_Click(object sender, EventArgs e)
+        {
+            //intput รับค่าจาก Textbox
+            string name = txtName.Text;
+            int age = Convert.ToInt32(txtAge.Text);
+            // 1 double height = Convert.ToDouble(txtHeight.Text);
+            // 1 double weight = Convert.ToDouble(txtWeight.Text);
+            //2   double height = 0;
+            //2     if (double.TryParse(txtHeight.Text,out height)==false)
+            //2     {
+            //2          MessageBox.Show("กรอกข้อมูลไม่ถูกต้ง", "เกิดข้อผิดพลาด");
+            //2         return; //ค่าในif เป็นจริง ให้จบการทำงาน
+            //2     }
+            //2     double weight;
+            //2     if (double.TryParse(txtWeight.Text,out weight)==false)
+            //2     {
+            //2         MessageBox.Show("กรอกข้อมูลไม่ถูกต้ง", "เกิดข้อผิดพลาด");
+            //2         return; //ค่าในif เป็นจริง ให้จบการทำงาน
+            //2     }
+
+            double height = 0;
+            double weight = 0;
+            if (CheckDouble(txtWeight, txtHeight)) //เรียกใช้Methodแบบคืนค่าเป็น True/false
+                height = Convert.ToInt32(txtHeight.Text);
+                weight = Convert.ToInt32(txtWeight.Text);
+
+
+            //Process คำนวณค่าBMI
+            //double bmi = weight / Math.Pow(height / 100, 2);
+            double bmi = BMI(height, weight);   //เรียกใช้ method BMI
+            //Output แสดงค่า BMI
+            lblResult.Text = bmi.ToString();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            clearForm();
+        }
+
+        private void clearForm()
+        {
+            txtAge.TextAlign = HorizontalAlignment.Right; //จัดชิดขวา
+            txtHeight.TextAlign = HorizontalAlignment.Right;
+            txtWeight.TextAlign = HorizontalAlignment.Right;
+            txtName.Text = "";
+            txtAge.Text = "0";
+            txtHeight.Text = "0";
+            txtWeight.Text = "0";
+            txtName.Focus();
+        }
+
+        private void IntputDataValid(TextBox name, TextBox age)
+        {
+            if (name.Text.Length!=0)
+            {
+                name.ForeColor = Color.DarkGreen;
+            }
+            else
+            {
+                name.ForeColor = Color.Gray;
+            }
+            if (Convert.ToInt32(age.Text) >=0)
+            {
+                age.ForeColor = Color.DarkGreen;
+            }
+            else
+            {
+                age.ForeColor = Color.Gray;
+            }
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+            IntputDataValid(txtName, txtAge);
+        }
+
+        private void txtAge_TextChanged(object sender, EventArgs e)
+        {
+            IntputDataValid(txtName, txtAge);
+        }
+
+        private double BMI(double h, double w) //Method BMI
+        { double bmi = w/Math.Pow(h/100, 2);
+            return bmi;
+            //แบบสั้น
+           // return w / Math.Pow(h / 100, 2);
+            
+        }
+
+        private bool CheckDouble(TextBox txtW, TextBox txtH) //สำหรับตรวจสอบข้อมูล
+        {
+            double w = 0;
+            double h = 0;
+            if (double.TryParse(txtW.Text,out w)||(double.TryParse(txtH.Text,out h)))
+            {
+                 MessageBox.Show("กรอกข้อมูลไม่ถูกต้ง", "เกิดข้อผิดพลาด");
+                return false;
+            }
+           
+               
+           
+            return true;
+        }
+    }
+}
